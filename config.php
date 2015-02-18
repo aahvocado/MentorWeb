de7a293a115e73d76cf39fdb3da7293c56452d7d
Mitchell Cox(mitchellcox5) <mitchellcox5@gmail.com>
2015-02-18 16:05:10 -0500

    repurposed db_credentials to be config file, adde inTestMode flag.

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+	$inTestMode = false;
+	$db_host = "db.cip.gatech.edu";
+	$db_database = "CONTRIB_mentoringweb";
+	$db_username = "contrib_mentorin";
+	$db_password = "LyMBy6EFdRzF7HF6";
+	/*
+	if(!$inTestMode){
+		$db_database = "CONTRIB_mentoringweb_test";
+	}
+	*/
+?>

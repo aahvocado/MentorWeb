21412113d7658e7c1e4852b12a5f42af15f44370
Mitchell Cox(mitchellcox5) <mitchellcox5@gmail.com>
2015-02-18 16:19:34 -0500

    Making dummy config file

--- config.php
+++ config.php
@@ -1,12 +1,11 @@
 <?php
 	$inTestMode = false;
-	$db_host = "db.cip.gatech.edu";
-	$db_database = "CONTRIB_mentoringweb";
-	$db_username = "contrib_mentorin";
-	$db_password = "LyMBy6EFdRzF7HF6";
-	/*
-	if(!$inTestMode){
-		$db_database = "CONTRIB_mentoringweb_test";
+	$db_host = "serverHostAddress";
+	$db_database = "database";
+	$db_username = "databaseUsername";
+	$db_password = "databaseUserPassword";
+	
+	if($inTestMode){
+		$db_database = "databaseTest";
 	}
-	*/
 ?>
